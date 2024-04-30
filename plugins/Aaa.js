0bd68ec7dcc93180869a49cde9185a97dffc1234
BOTGATITO <149286603+BOTGATITO@users.noreply.github.com>
2024-04-29 23:44:45 -0500

    Update Aaa.js

--- plugins/Aaa.js
+++ plugins/Aaa.js
@@ -5,7 +5,7 @@ let a = ps.getRandom()
 let b
 do b = ps.getRandom()
 while (b === a)
-m.reply(`*${toM(a)}, *_le tocó donar una sala estás de mala suerte hoy bájate con todo lo que tengas_😾💥`, null, {
+m.reply(`*${toM(a)},* _le tocó donar una sala estás de mala suerte hoy bájate con todo lo que tengas_😾💥`, null, {
 mentions: [a, b]
 })}
 handler.help = ['formarpareja']
