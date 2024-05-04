ed3c35926a7262128ec169f97433a80f87aa7301
BOTGATITO <149286603+BOTGATITO@users.noreply.github.com>
2024-05-03 19:35:37 -0500

    Update _autoresponder.js

--- plugins/_autoresponder.js
+++ plugins/_autoresponder.js
@@ -1,4 +1,4 @@
-import { sticker } from '../../lib/sticker.js'
+import { sticker } from '../lib/sticker.js'
 let handler = m => m
 
 handler.all = async function (m, {conn}) {
@@ -7,7 +7,7 @@ let chat = global.db.data.chats[m.chat]
 if (m.mentionedJid.includes(this.user.jid) && m.isGroup && !chat.isBanned) {
 let stiker = await sticker(imagen1, false, global.packname, global.author)  
 this.sendFile(m.chat, stiker, 'sticker.webp', null, m, false, { 
-contextInfo: { externalAdReply: { title: '𝐄́𝐋𝐈𝐓𝐄 𝐁𝐎𝐓', body: '@Jeffomx15', sourceUrl: `https://instagram.com/jeffomx15?igshid=NTc4MTIwNjQ2YQ==`, thumbnail: imagen2}}})}
+contextInfo: { externalAdReply: { title: 'ProyectoX', body: 'ProyectoX', sourceUrl: md, thumbnail: imagen2}}})}
     
 return !0 }
 export default handler
