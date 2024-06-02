e6f4012b74cf76faa137de6dfde969f898900899
BOTGATITO <149286603+BOTGATITO@users.noreply.github.com>
2024-06-02 09:35:31 -0500

    Update sticker-sticker.js

--- plugins/sticker-sticker.js
+++ plugins/sticker-sticker.js
@@ -1,54 +1,50 @@
-import { sticker } from '../lib/sticker.js'
-import uploadFile from '../lib/uploadFile.js'
-import uploadImage from '../lib/uploadImage.js'
-import { webp2png } from '../lib/webp2mp4.js'
+import {sticker} from '../lib/sticker.js';
+import uploadFile from '../lib/uploadFile.js';
+import uploadImage from '../lib/uploadImage.js';
+import {webp2png} from '../lib/webp2mp4.js';
 
-let handler = async (m, { conn, args, usedPrefix, command }) => {
+const handler = async (m, {conn, args, usedPrefix, command}) => {
+  if (usedPrefix == 'a' || usedPrefix == 'A') return;
+  let stiker = false;
+  const user = db.data.users[m.sender];
+  try {
+    const q = m.quoted ? m.quoted : m;
+    const mime = (q.msg || q).mimetype || q.mediaType || '';
+    if (/webp|image|video/g.test(mime)) {
+      const img = await q.download?.();
+      if (!img) throw `*responda a una imagen o video*`;
+      let out;
+      try {
+        stiker = await sticker(img, false, global.packname, global.author);
+      } catch (e) {
+        console.error(e);
+      } finally {
+        if (!stiker) {
+          if (/webp/g.test(mime)) out = await webp2png(img);
+          else if (/image/g.test(mime)) out = await uploadImage(img);
+          else if (/video/g.test(mime)) out = await uploadFile(img);
+          if (typeof out !== 'string') out = await uploadImage(img);
+          stiker = await sticker(false, out, global.packname, global.author);
+        }
+      }
+    } else if (args[0]) {
+      if (isUrl(args[0])) stiker = await sticker(false, args[0], global.packname, global.author);
+      else return m.reply('*enlace no valido*');
+    }
+  } catch (e) {
+    console.error(e);
+    if (!stiker) stiker = e;
+  } finally {
+    if (stiker) conn.sendFile(m.chat, stiker, 'sticker.webp', '', m);
+    else throw '*error*';
+  }
+};
+handler.help = ['sticker'];
+handler.tags = ['sticker'];
+handler.command = ['sticker', 's'] 
 
-let stiker = false
-try {
-let q = m.quoted ? m.quoted : m
-let mime = (q.msg || q).mimetype || q.mediaType || ''
-if (/webp|image|video/g.test(mime)) {
-if (/video/g.test(mime)) if ((q.msg || q).seconds > 8) return m.reply(`*EL VIDEO NO PUEDE DURA MAS DE 7 SEGUNDOS*`)
-let img = await q.download?.()
-
-if (!img) throw `uso del comando incorrecto responde una imagen u video con ${usedPrefix + command}`
-
-let out
-try {
-stiker = await sticker(img, false, global.packname, global.author)
-await conn.reply(m.chat, `> creando tu sticker espere un momento..`, m, fake,)
-} catch (e) {
-console.error(e)
-} finally {
-if (!stiker) {
-if (/webp/g.test(mime)) out = await webp2png(img)
-else if (/image/g.test(mime)) out = await uploadImage(img)
-else if (/video/g.test(mime)) out = await uploadFile(img)
-if (typeof out !== 'string') out = await uploadImage(img)
-stiker = await sticker(false, out, global.packname, global.author)
-}}
-} else if (args[0]) {
-if (isUrl(args[0])) stiker = await sticker(false, args[0], global.packname, global.author)
-
-else return m.reply(`URL invalido`)
-  
-}
-} catch (e) {
-console.error(e)
-if (!stiker) stiker = e
-} finally {
-if (stiker) conn.sendFile(m.chat, stiker, 'sticker.webp', '',m, true, { contextInfo: { 'forwardingScore': 200, 'isForwarded': false, externalAdReply:{ showAdAttribution: false, title: wm, body: ``, mediaType: 2, sourceUrl: [nna, nn, md, yt].getRandom(), thumbnail: imagen1}}}, { quoted: m })
-
-else throw `lo uso mal responde a una imagen u video con *${usedPrefix + command}*`
-
-}}
-handler.help = ['stiker (caption|reply media)', 'stiker <url>', 'stikergif (caption|reply media)', 'stikergif <url>']
-handler.tags = ['sticker']
-handler.command = /^s(tic?ker)?(gif)?(wm)?$/i
-
-export default handler
+export default handler;
 
 const isUrl = (text) => {
-return text.match(new RegExp(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&/=]*)(jpe?g|gif|png)/, 'gi'))}
+  return text.match(new RegExp(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&/=]*)(jpe?g|gif|png)/, 'gi'));
+};
