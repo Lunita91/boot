bd9682098781e635a29c00c6a2f0e5918db6eed6
BOTGATITO <149286603+BOTGATITO@users.noreply.github.com>
2024-06-02 09:57:43 -0500

    Update menuimg.js

--- plugins/menuimg.js
+++ plugins/menuimg.js
@@ -371,7 +371,7 @@ let menu = `╔═════════════════╗
 ╰ ∙∙∙∙∙∙∙∙∙∙∙∙∙∙∙∙∙ ∙ ∙
  `.trim()
  
-const img = ['https://telegra.ph/file/7efee0ddae0b0a20910c9.jpg']
+const img = ['https://telegra.ph/file/4be624576f970e0e1a413.jpg']
 await conn.sendMessage(m.chat, { image: { url: img.getRandom() }, gifPlayback: true, caption: menu, mentions: [m.sender, global.conn.user.jid] }, { quoted: fkontak })
 	
 } catch (e) {
