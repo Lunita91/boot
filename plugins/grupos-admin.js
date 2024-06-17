13a9ccb25e61b448ea7ddffb2265e055cdf9d435
BOTGATITO <149286603+BOTGATITO@users.noreply.github.com>
2024-06-17 01:00:45 -0500

    Update grupos-admin.js

--- plugins/grupos-admin.js
+++ plugins/grupos-admin.js
@@ -171,7 +171,7 @@ let oi = `» 🔱 ${lenguajeGB['smsAddB5']()} ${pesan}`
 let teks = `${lenguajeGB['smstagaa']()}\n\n${oi}\n\n`
 for (let mem of participants) {
 teks += `➥🔱@${mem.id.split('@')[0]}\n`}
-teks += `*NIKOLBOT*`
+teks += ``
 await conn.sendMessage(m.chat, { text: teks, mentions: participants.map(a => a.id) }, )    
 break
     
