6f165d5a4b1d63f9c77d1449996113974b8f52dc
Lunita91 <157089185+Lunita91@users.noreply.github.com>
2024-08-04 20:22:03 -0500

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const { name, author } = require(join(__dirname, "./package.json"));
 const { say } = cfonts;
 const rl = createInterface(process.stdin, process.stdout);
 
-say('PROYECTO\nX', {
+say('SISKED\nBOT', {
 font: 'chrome',
 align: 'center',
 gradient: ['red', 'magenta']})
